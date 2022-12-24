3eb29568e457d343d6926987648ff0d100b61181
Ramiro_Quai <ramiro.tello.s@gmail.com>
2022-12-23 18:02:07 -0600

    Se agregan mas ejemplos

---
+++ 01.EstudioPersonal/09.TypeScript/TSProy/app/herencia.ts
@@ -0,0 +1,102 @@
+class Persona{
+    private nombre:string;
+    apellido:string;
+    edad:number;
+
+    constructor(_nombre:string, _apellido:string, _edad:number){
+        this.nombre = _nombre;
+        this.apellido = _apellido;
+        this.edad = _edad;
+    }
+
+    protected ObtenerNombre():String{
+        return this.nombre;
+    }
+
+    ImprimirNombre():String{
+        return this.nombre;
+    }
+}
+
+class Empleado extends Persona{
+    idEmpleado:number;
+
+    constructor(_idEmpleado:number,_nombre:string, _apellido:string, _edad:number){
+        super(_nombre, _apellido, _edad);
+        this.idEmpleado = _idEmpleado;
+        
+    }
+
+}
+
+class Estudiante extends Persona{
+    idEstudiante:number;
+    constructor(_idEstudiante:number,_nombre:string, _apellido:string, _edad:number){
+        super(_nombre, _apellido, _edad);
+        this.idEstudiante = _idEstudiante;
+    }
+}
+
+let empleado = new Empleado(1,"Ramiro desde clase con variable privada", "Tello", 36);
+console.log( empleado.ImprimirNombre());
+
+class Cliente {
+    
+    private _nombre? : string;
+    public get nombre() : string {
+        if (this._nombre) {
+            return this._nombre;    
+        }
+        return "Valor por defecto";
+        
+    }
+    public set nombre(v : string) {
+        this._nombre = v;
+    }
+    
+    constructor(nombreCliente?:string) {       
+        this._nombre = nombreCliente; 
+    }
+}
+
+let cliente =  new Cliente();
+console.log(cliente.nombre);
+
+///Abstracto
+abstract class Animal{
+    abstract Ruido():void;
+}
+class Gato extends Animal{
+    Ruido(): void {
+        console.log("Miau")
+    }
+}
+class Perro extends Animal{
+    Ruido(): void {
+        console.log("Guau")
+    }
+    brinca(){
+
+    }
+}
+
+let perro = new Perro()
+perro.Ruido();
+
+//Interfaces
+interface IMatematicas{
+    total?:number;
+    Sumar(numUno:number, numDos:number):number;
+    Restar(numUno:number, numDos:number):number;
+}
+
+class Utilidad implements IMatematicas{
+    total?: number;
+    public Sumar(numUno: number, numDos: number): number {
+        this.total =numUno + numDos;
+        return this.total;
+    }
+    public Restar(numUno: number, numDos: number): number {
+        return numUno-numDos;
+    }
+}
