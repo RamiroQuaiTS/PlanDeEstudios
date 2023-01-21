be0565e28c927f61717e7d95d945e9bf3a400d74
Ramiro_Quai <ramiro.tello.s@gmail.com>
2023-01-20 18:11:35 -0600

    se agrega nuevo proyecto de anguar

---
+++ 01.EstudioPersonal/09.TypeScript/TSUdemy/src/con-js/saludos.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {string} mensaje 
+ * @param {number} numero
+ * @return {string}
+ */
+export function holaMundos(mensaje, numero) {
+    console.log(`Hola mundo ${mensaje} ${numero}`)
+
+    return mensaje
+}
